d7f9979e800307a8f0b6795353066618f0947bd9
Val Snow <valeriesnowuwu@gmail.com>
2023-08-13 20:22:42 +1000

    Fixed issue when BTKUILib is not installed (BeautifyParticles)

--- BeautifyParticles/Main.cs
+++ BeautifyParticles/Main.cs
@@ -1,23 +1,16 @@
 ﻿using MelonLoader;
-using System;
 using System.Linq;
 using UnityEngine;
-using BTKUILib.UIObjects;
-using BTKUILib;
-using System.Reflection;
 
 namespace BeautifyParticles
 {
     public class Main : MelonMod
     {
-        //  Root page of mod
-        private Page page;
-
-        private MelonPreferences_Category preferencesCategory;
-        private MelonPreferences_Entry<bool> autoBeautify;
-        private MelonPreferences_Entry<bool> setFacing;
-        private MelonPreferences_Entry<bool> setRoll;
-        private MelonPreferences_Entry<bool> onlyView;
+        public MelonPreferences_Category preferencesCategory;
+        public MelonPreferences_Entry<bool> autoBeautify;
+        public MelonPreferences_Entry<bool> setFacing;
+        public MelonPreferences_Entry<bool> setRoll;
+        public MelonPreferences_Entry<bool> onlyView;
 
         public override void OnInitializeMelon() //Sets up UI Expansion Kit
         {
@@ -30,50 +23,15 @@ namespace BeautifyParticles
             setRoll = preferencesCategory.CreateEntry<bool>("setRoll", true);
             onlyView = preferencesCategory.CreateEntry<bool>("onlyView", true);
 
-
             //  Check if BTKUILib is installed
             if (RegisteredMelons.Any(it => it.Info.Name == "BTKUILib"))
             {
                 LoggerInstance.Msg("Found BTKUILib");
-
-                QuickMenuAPI.PrepareIcon("BeautifyParticles", "BeautifyParticlesIcon", Assembly.GetExecutingAssembly().GetManifestResourceStream("BeautifyParticles.Icon.png"));
-                //  Initialise UI
-                page = new Page("BeautifyParticles", "Beautify Particles", true, "BeautifyParticlesIcon");
-                page.MenuTitle = "Beautify Particles";
-                page.MenuSubtitle = "Improve the look of particles";
-
-                //  Basic settings
-                var basicSettings = page.AddCategory("Basic Settings");
-
-                var manualSet = basicSettings.AddButton("Beautify Particles!", "BeautifyParticlesIcon", "Make them pretty");
-                manualSet.OnPress = SetParticles;
-                
-                var autoBeautifyToggle = basicSettings.AddToggle("Auto-Beautify Worlds", "Automatically beautify worlds on join", autoBeautify.Value);
-                autoBeautifyToggle.OnValueUpdated += b =>
-                {
-                    autoBeautify.Value = b;
-                };
-
-                //  Advanced settings
-                var advancedSettings = page.AddCategory("Advanced Settings");
-
-                var setFacingToggle = advancedSettings.AddToggle("Set to face camera", "Default: true", setFacing.Value);
-                setFacingToggle.OnValueUpdated += b =>
-                {
-                    setFacing.Value = b;
-                };
-
-                var setRollToggle = advancedSettings.AddToggle("Disallow roll", "Default: true", setRoll.Value);
-                setRollToggle.OnValueUpdated += b =>
-                {
-                    setRoll.Value = b;
-                };
-
-                var onlyViewToggle = advancedSettings.AddToggle("Only when set to view", "Default: true", onlyView.Value);
-                onlyViewToggle.OnValueUpdated += b =>
-                {
-                    onlyView.Value = b;
-                };
+                UIManager.setupBTKUILib(this);
+            }
+            else
+            {
+                LoggerInstance.Warning("BTKUILib is recommended.");
             }
         }
 
---
+++ BeautifyParticles/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using BTKUILib;
+using BTKUILib.UIObjects;
+using MelonLoader;
+using System;
+using System.Reflection;
+
+namespace BeautifyParticles
+{
+    public static class UIManager
+    {
+        //  Required to avoid errors if dependency is not installed ugh
+        public static void setupBTKUILib(Main m)
+        {
+            QuickMenuAPI.PrepareIcon("BeautifyParticles", "BeautifyParticlesIcon", Assembly.GetExecutingAssembly().GetManifestResourceStream("BeautifyParticles.Icon.png"));
+            //  Initialise UI
+            var page = new Page("BeautifyParticles", "Beautify Particles", true, "BeautifyParticlesIcon");
+            page.MenuTitle = "Beautify Particles";
+            page.MenuSubtitle = "Improve the look of particles";
+
+            //  Basic settings
+            var basicSettings = page.AddCategory("Basic Settings");
+
+            var manualSet = basicSettings.AddButton("Beautify Particles!", "BeautifyParticlesIcon", "Make them pretty");
+            manualSet.OnPress = m.SetParticles;
+
+            var autoBeautifyToggle = basicSettings.AddToggle("Auto-Beautify Worlds", "Automatically beautify worlds on join", m.autoBeautify.Value);
+            autoBeautifyToggle.OnValueUpdated += b =>
+            {
+                m.autoBeautify.Value = b;
+            };
+
+            //  Advanced settings
+            var advancedSettings = page.AddCategory("Advanced Settings");
+
+            var setFacingToggle = advancedSettings.AddToggle("Set to face camera", "Default: true", m.setFacing.Value);
+            setFacingToggle.OnValueUpdated += b =>
+            {
+                m.setFacing.Value = b;
+            };
+
+            var setRollToggle = advancedSettings.AddToggle("Disallow roll", "Default: true", m.setRoll.Value);
+            setRollToggle.OnValueUpdated += b =>
+            {
+                m.setRoll.Value = b;
+            };
+
+            var onlyViewToggle = advancedSettings.AddToggle("Only when set to view", "Default: true", m.onlyView.Value);
+            onlyViewToggle.OnValueUpdated += b =>
+            {
+                m.onlyView.Value = b;
+            };
+        }
+    }
+}
